fd9021e691502c5a316d3570dca2f8f3cac0be08
MarsBighead <duanmhy@126.com>
2016-12-17 18:05:46 +0800

    remove email information

--- send.js
+++ send.js
@@ -3,17 +3,17 @@ options ={
     from: '"xxxx Group" <register-noreply@xxx.com>', // sender address
     to:{
        email :'hbu@localhost', // list of receivers
-       cc :'duanmhy@139.com', 
-       bcc:'duanmhy@126.com', 
-       name: "TestToger",
+       cc :'xxxxx@xxx.com', 
+       bcc:'xxxx@gmail.com', 
+       name: "Test",
        surname: "T"
     },
     subject: "Please active your account", // Subject line
     template: "reg_active.ejs"
 };
 data ={
-  /*name: "Paul",
-  surname: "Duan",*/
+  /*name: "Bush",
+  surname: "Jeb",*/
   name: options.to.name,
   surname: options.to.surename,
   id: "2_reg_active_id"
